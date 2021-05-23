033b126df452a299241120449e84fa72d87ab5ec
zeydba <81634439+zeydba@users.noreply.github.com>
2021-05-23 20:16:52 +0200

    Update camera.py

--- projet/simulator/graphics/camera.py
+++ projet/simulator/graphics/camera.py
@@ -1,11 +1,22 @@
 from ..utils.vector import Vector2
 
 
-class Camera:
+class ICamera:
     def __init__(self, screen_size):
         self.screen_size = screen_size
         self.position = Vector2(0, 0)
         self.scale = 1
+        
+        
+    def to_screen_coords(self, position):
+        """ Converts the world-coordinate position to a screen-coordinate. """
+        raise NotImplementedError
+
+    def from_screen_coords(self, position):
+        """ Converts the screen-coordinate position to a world-coordinate. """
+        raise NotImplementedError
+        
+class Camera(ICamera):
 
     def to_screen_coords(self, position):
         """ Converts the world-coordinate position to a screen-coordinate. """
@@ -15,4 +26,4 @@ class Camera:
     def from_screen_coords(self, position):
         """ Converts the screen-coordinate position to a world-coordinate. """
         coord_screen = 1/self.scale * (position + self.position*self.scale - self.screen_size/2)
-        return coord_screen 
+        return coord_screen
